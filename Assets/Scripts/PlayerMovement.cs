fed0217099a0850f1ad31217820b0236bb0590b9
meeteedee <meepoohfisho@gmail.com>
2026-04-24 11:33:16 +0700

    EnemyBounce

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    
+    //walk left-right
+    float move;
+    [SerializeField] float speed;
+    
+    //Jump
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isjumping;
+    // Start is called once beforethe  first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //walk left-right
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+        
+        //Jump
+        if (Input.GetButtonDown("Jump") && !isjumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump!"); //for debugging
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = true;
+        }
+    }
+}
